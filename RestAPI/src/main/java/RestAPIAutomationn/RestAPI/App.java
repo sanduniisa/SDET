a79e8a938b7173aadf559bdaf1cf0742960e08c0
sanduniisa <sanduniisa@gmail.com>
2024-07-06 12:08:48 +0400

    removal RestAPI folder

---
+++ RestAPI/src/main/java/RestAPIAutomationn/RestAPI/App.java
@@ -0,0 +1,13 @@
+package RestAPIAutomationn.RestAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
